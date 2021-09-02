e8fb522f8854d5b7ddbf3a890dede8953583c111
maya <maya.salcedo07@gmail.com>
2021-09-02 11:28:17 +0300

    cloned the react-node-express template and deleted the .git

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import HomeScreen from '../src/screens/HomeScreen';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>Learn React</header>
+      <HomeScreen />
+    </div>
+  );
+}
+
+export default App;
