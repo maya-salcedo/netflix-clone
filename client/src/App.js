f4964bb2ba1c05fd6a0327c46653bc64291a8562
maya <maya.salcedo07@gmail.com>
2021-09-02 12:28:15 +0300

    added the routes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
-import HomeScreen from '../src/screens/HomeScreen';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <header>Learn React</header>
-      <HomeScreen />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/signin">
+          <p>This is sign in page</p>
+        </Route>
+        <Route path="/signup">
+          <p>This is sign up page</p>
+        </Route>
+        <Route path="/browse">
+          <p>This is browse page</p>
+        </Route>
+        <Route path="/">
+          <p>This is home page</p>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
