a35aab49360169bc3080207f5066ed69689aa594
maya <maya.salcedo07@gmail.com>
2021-09-06 22:15:36 +0300

    styled the jumbotron

--- client/src/components/jumbotron/styles/jumbotron.js
+++ client/src/components/jumbotron/styles/jumbotron.js
@@ -6,17 +6,41 @@ export const Inner = styled.div`
   justify-content: space-between;
   flex-direction: ${({ direction }) => direction};
   max-width: 1100px;
+  margin: auto;
+  width: 100%;
 
   @media (max-width: 1000px) {
     flex-direction: column;
   }
 `;
 
-export const Item = styled.div`
-  display: flex;
+export const Pane = styled.div`
+  width: 50%;
+
+  @media (max-width: 1000px) {
+    width: 100%;
+    padding: 0 45px;
+    text-align: center;
+  }
 `;
 
-export const Container = styled.section``;
+export const Item = styled.div`
+  display: flex;
+  border-bottom: 8px solid #222;
+  padding: 50px 5%;
+  color: white;
+  overflow: hidden;
+`;
+
+export const Container = styled.section`
+  background-color: black;
+
+  @media (max-width: 1000px) {
+    ${Item}:last-of-type h2 {
+      margin-bottom: 50px;
+    }
+  }
+`;
 
 export const Title = styled.h1`
   font-size: 50px;
