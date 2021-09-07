991c6051631aa08d1fc31028dcb557a2ba6298df
maya <maya.salcedo07@gmail.com>
2021-09-07 14:35:34 +0300

    created opt-form component which is rendered in component faqs

---
+++ client/src/components/opt-form/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Button, Container, Input, Text } from './styles/opt-form';
+
+export default function OptForm({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
+}
+
+OptForm.Input = function OptFormInput({ ...restProps }) {
+  return <Input {...restProps} />;
+};
+
+OptForm.Button = function OptFormButton({ children, ...restProps }) {
+  return <Button {...restProps}>{children}</Button>;
+};
+
+OptForm.Text = function OptFormText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>;
+};
