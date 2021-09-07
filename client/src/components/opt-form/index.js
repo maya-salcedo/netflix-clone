927bc95759a8aa6a48e7c2808685c04ec4810073
maya <maya.salcedo07@gmail.com>
2021-09-07 14:44:57 +0300

    added elements to the form

--- client/src/components/opt-form/index.js
+++ client/src/components/opt-form/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Button, Container, Input, Text } from './styles/opt-form';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGreaterThan } from '@fortawesome/free-solid-svg-icons';
 
 export default function OptForm({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
@@ -10,7 +12,11 @@ OptForm.Input = function OptFormInput({ ...restProps }) {
 };
 
 OptForm.Button = function OptFormButton({ children, ...restProps }) {
-  return <Button {...restProps}>{children}</Button>;
+  return (
+    <Button {...restProps}>
+      {children} <FontAwesomeIcon icon={faGreaterThan} />
+    </Button>
+  );
 };
 
 OptForm.Text = function OptFormText({ children, ...restProps }) {
