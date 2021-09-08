c5f5ab33a31847b77f78ff78e73a1692c71540c1
maya <maya.salcedo07@gmail.com>
2021-09-08 09:44:01 +0300

    built a compound component in home page

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Feature, OptForm } from '../components';
 import { FaqsContainer } from '../containers/faqs';
 import { FooterContainer } from '../containers/footer';
 import { HeaderContainer } from '../containers/header';
@@ -7,7 +8,24 @@ import { JumbotronContainer } from '../containers/jumbotron';
 export default function Home() {
   return (
     <>
-      <HeaderContainer />
+      <HeaderContainer>
+        <OptForm>
+          <Feature>
+            <Feature.Title>
+              Unlimited films, TV programmes and more.
+            </Feature.Title>
+            <Feature.Subtitle>
+              Watch anywhere. Cancel at any time.
+            </Feature.Subtitle>
+            <OptForm.Input placeholder="Email Address" />
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Text>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </OptForm.Text>
+          </Feature>
+        </OptForm>
+      </HeaderContainer>
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
