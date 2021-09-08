8c0b57f56e04735c833539a27edc6fcfa589aaf3
maya <maya.salcedo07@gmail.com>
2021-09-08 09:47:48 +0300

    fixed the structure of feature component in home page

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -9,22 +9,22 @@ export default function Home() {
   return (
     <>
       <HeaderContainer>
-        <OptForm>
-          <Feature>
-            <Feature.Title>
-              Unlimited films, TV programmes and more.
-            </Feature.Title>
-            <Feature.Subtitle>
-              Watch anywhere. Cancel at any time.
-            </Feature.Subtitle>
+        <Feature>
+          <Feature.Title>
+            Unlimited films, TV programmes and more.
+          </Feature.Title>
+          <Feature.Subtitle>
+            Watch anywhere. Cancel at any time.
+          </Feature.Subtitle>
+          <OptForm>
             <OptForm.Input placeholder="Email Address" />
             <OptForm.Button>Try it now</OptForm.Button>
             <OptForm.Text>
               Ready to watch? Enter your email to create or restart your
               membership.
             </OptForm.Text>
-          </Feature>
-        </OptForm>
+          </OptForm>
+        </Feature>
       </HeaderContainer>
       <JumbotronContainer />
       <FaqsContainer />
