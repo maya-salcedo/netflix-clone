77f104d86eb73cec7433d8187c925c135b0f8698
maya <maya.salcedo07@gmail.com>
2021-09-08 14:22:31 +0300

    added footer to signin page

--- client/src/pages/signin.js
+++ client/src/pages/signin.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Form } from '../components';
+import { FooterContainer } from '../containers/footer';
 import { HeaderContainer } from '../containers/header';
 
 export default function Signin() {
@@ -17,34 +18,37 @@ export default function Signin() {
   };
 
   return (
-    <HeaderContainer>
-      <Form>
-        <Form.Title>Sign In</Form.Title>
-        {error && <Form.Error>{error}</Form.Error>}
-        <Form.Base onSubmit={handleSignin} method="POST">
-          <Form.Input
-            placeholder="Email address"
-            value={emailAddress}
-            onChange={({ target }) => setEmailAddress(target.value)}
-          />
-          <Form.Input
-            placeholder="Password"
-            type="password"
-            autocomplete="off"
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
-          />
-          <Form.Submit disabled={isInvalid} type="submit">
-            Sign In
-          </Form.Submit>
-          <Form.Text>
-            New to Netflix? <Form.Link to="/signup">Sign Up now.</Form.Link>
-          </Form.Text>
-          <Form.TextSmall>
-            This page is protected by Google reCAPTCHA.
-          </Form.TextSmall>
-        </Form.Base>
-      </Form>
-    </HeaderContainer>
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSignin} method="POST">
+            <Form.Input
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              placeholder="Password"
+              type="password"
+              autocomplete="off"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign In
+            </Form.Submit>
+            <Form.Text>
+              New to Netflix? <Form.Link to="/signup">Sign Up now.</Form.Link>
+            </Form.Text>
+            <Form.TextSmall>
+              This page is protected by Google reCAPTCHA.
+            </Form.TextSmall>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
   );
 }
