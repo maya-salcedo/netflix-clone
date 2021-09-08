7d3d2a262634533f18b2d6d3b907eb496121dd78
maya <maya.salcedo07@gmail.com>
2021-09-08 10:11:30 +0300

    hooked up the app to Firebase

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { GlobalStyle } from './global-styles';
+import { FirebaseContext } from './context/firebase';
+
+const config = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+};
+
+const firebase = window.firebase.initializeApp(config);
 
 ReactDOM.render(
   <>
-    <GlobalStyle />
-    <App />
+    <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+      <GlobalStyle />
+      <App />
+    </FirebaseContext.Provider>
   </>,
   document.getElementById('root')
 );
