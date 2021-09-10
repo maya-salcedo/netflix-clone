36275bd1064ea695e2d4f6c3324fabe1eb618f86
maya <maya.salcedo07@gmail.com>
2021-09-10 11:24:46 +0300

    hydrated firestore with data

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,9 @@ import { config } from './utils/firebaseConfig';
 import { GlobalStyle } from './global-styles';
 import { FirebaseContext } from './context/firebase';
 
+//Uncomment after seeding to avoid duplicates
+//import { seedDatabase } from './seed';
+
 const firebase = window.firebase.initializeApp(config);
 
 ReactDOM.render(
