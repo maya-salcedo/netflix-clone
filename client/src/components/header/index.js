d79820b01f4bbebb9f2b88f571b1cfaa51fd4970
maya <maya.salcedo07@gmail.com>
2021-09-10 07:02:30 +0300

    added Header to the Browse Containe

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { Link as ReachRouterLink } from 'react-router-dom';
-import { Background, ButtonLink, Container, Logo } from './styles/header';
+import {
+  Background,
+  ButtonLink,
+  Container,
+  Group,
+  Link,
+  Logo,
+  Text,
+} from './styles/header';
 
 export default function Header({ bg = true, children, ...restProps }) {
   return bg ? <Background {...restProps}>{children}</Background> : children;
@@ -10,12 +18,24 @@ Header.Frame = function HeaderFrame({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
 };
 
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+  return <Group {...restProps}>{children}</Group>;
+};
+
 Header.Logo = function HeaderLogo({ to, ...restProps }) {
   return (
     <ReachRouterLink to={to}>
       <Logo {...restProps} />
     </ReachRouterLink>
   );
+};
+
+Header.Text = function HeaderText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>;
+};
+
+Header.Link = function HeaderFunction({ children, ...restProps }) {
+  return <Link {...restProps}>{children}</Link>;
 };
 
 Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
--- client/src/containers/browse.js
+++ client/src/containers/browse.js
@@ -1,17 +1,43 @@
 import React, { useState } from 'react';
+import { Header } from '../components';
 import { FooterContainer } from './footer';
 import { SelectProfileContainer } from './profiles';
+import * as ROUTES from '../constants/routes';
 
 export default function BrowseContainer() {
   const [profile, setProfile] = useState({});
+  const [category, setCategory] = useState('series');
 
   const user = {
     displayName: 'Maya',
     photoURL: '1',
   };
+
   return profile.displayName ? (
     <>
-      <p>Browse Container</p>
+      <Header src="joker" dontShowOnSmallViewPort>
+        <Header.Frame>
+          <Header.Group>
+            <Header.Logo
+              to={ROUTES.HOME}
+              src="/images/misc/logo.png"
+              alt="Netflix"
+            ></Header.Logo>
+            <Header.Link
+              active={category === 'series' ? 'true' : 'false'}
+              onClick={() => setCategory('series')}
+            >
+              Series
+            </Header.Link>
+            <Header.Link
+              active={category === 'films' ? 'true' : 'false'}
+              onClick={() => setCategory('films')}
+            >
+              Films
+            </Header.Link>
+          </Header.Group>
+        </Header.Frame>
+      </Header>
       <FooterContainer />
     </>
   ) : (
